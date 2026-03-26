817d30be550d8b6da01b137803f0feb2c5118537
maguy12 <kamangomaguy5@gmail.com>
2026-03-26 03:46:56 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,37 +1,45 @@
-import { loginWithGoogle, logout } from "../firebase/authService.js";
 import { auth } from "../firebase/config.js";
 
 import {
-  onAuthStateChanged
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// LOGIN
+const provider = new GoogleAuthProvider();
+
+// 🔐 LOGIN
 const btn = document.getElementById("googleLogin");
 
 if (btn) {
-  btn.onclick = async () => {
+  btn.addEventListener("click", async () => {
+
     try {
-      await loginWithGoogle();
+      await signInWithPopup(auth, provider);
+
+      // redirection après login
       window.location.href = "index.html";
-    } catch (e) {
-      alert("Erreur login");
-      console.error(e);
+
+    } catch (error) {
+      console.error("Erreur login:", error);
+      alert("Erreur connexion !");
     }
-  };
+
+  });
 }
 
-// PROTECTION
+// 🔁 REDIRECTION AUTO
 onAuthStateChanged(auth, (user) => {
 
-  // Si pas connecté → login
-  if (!user && !window.location.pathname.includes("login.html")) {
-    window.location.href = "login.html";
+  if (user && window.location.pathname.includes("login.html")) {
+    window.location.href = "index.html";
   }
 
 });
 
-// LOGOUT GLOBAL
+// 🚪 LOGOUT GLOBAL
 window.logoutUser = async () => {
-  await logout();
+  await signOut(auth);
   window.location.href = "login.html";
 };
