d0c2d6dd26f43c02d40aa5f04afc7abedf3c4342
maguy12 <kamangomaguy5@gmail.com>
2026-03-26 03:19:56 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,18 @@
-import { auth } from "../firebase/config.js";
-import {
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// ⏳ LOADER (3 secondes obligatoire)
+// ⏳ LOADER GLOBAL (3 secondes obligatoire)
 window.addEventListener("load", () => {
+
   setTimeout(() => {
     const loader = document.getElementById("loader");
-    if (loader) loader.style.display = "none";
-  }, 3000);
-});
 
-// 🔐 AUTH + USER UI
-onAuthStateChanged(auth, (user) => {
+    if (loader) {
+      loader.style.opacity = "0";
+      loader.style.transition = "0.5s";
 
-  // ❌ pas connecté → login
-  if (!user) {
-    if (!window.location.pathname.includes("login.html")) {
-      window.location.href = "login.html";
+      setTimeout(() => {
+        loader.style.display = "none";
+      }, 500);
     }
-    return;
-  }
 
-  // ✅ connecté → afficher info
-  const name = document.getElementById("userName");
-  const photo = document.getElementById("userPhoto");
-
-  if (name) name.innerText = user.displayName;
-  if (photo) photo.src = user.photoURL;
+  }, 3000);
 
 });
