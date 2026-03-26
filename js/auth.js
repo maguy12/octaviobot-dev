f562579d776d80447a54d5a4b47feded728771ce
maguy12 <kamangomaguy5@gmail.com>
2026-03-26 02:48:40 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,34 @@
 import { loginWithGoogle, logout } from "../firebase/authService.js";
+import { auth } from "../firebase/config.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // LOGIN
 const btn = document.getElementById("googleLogin");
 
 if (btn) {
   btn.onclick = async () => {
-    await loginWithGoogle();
-    window.location.href = "index.html";
+    try {
+      await loginWithGoogle();
+      window.location.href = "index.html";
+    } catch (e) {
+      alert("Erreur login");
+      console.error(e);
+    }
   };
 }
+
+// PROTECTION
+onAuthStateChanged(auth, (user) => {
+
+  // Si pas connecté → login
+  if (!user && !window.location.pathname.includes("login.html")) {
+    window.location.href = "login.html";
+  }
+
+});
 
 // LOGOUT GLOBAL
 window.logoutUser = async () => {
